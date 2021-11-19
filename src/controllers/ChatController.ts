f44f829eec0c4113b2818d8f66bae260aa1351d7
Rebel Guy <rebelguysminecraft@gmail.com>
2021-11-19 22:41:54 +1000

    Added schemas for API response objects

--- src/controllers/ChatController.ts
+++ src/controllers/ChatController.ts
@@ -2,9 +2,10 @@ import { Dependencies } from '@rebel/context/ContextProvider'
 import { buildPath } from '@rebel/controllers/BaseEndpoint'
 import { ChatItem } from '@rebel/models/chat'
 import ChatStore from '@rebel/stores/ChatStore'
+import { ApiSchema } from '@rebel/types'
 import { GET, Path, QueryParam } from "typescript-rest"
 
-type GetChatResponse = {
+type GetChatResponse = ApiSchema<1, {
   liveId: string
 
   // include the timestamp so it can easily be used for the next request
@@ -13,7 +14,7 @@ type GetChatResponse = {
   // whether the response contains partial chat data or all available chat data
   isPartial: boolean
   chat: ChatItem[]
-}
+}>
 
 @Path(buildPath('chat'))
 export class ChatController {
@@ -36,6 +37,7 @@ export class ChatController {
     const items = this.chatStore.chatItems.filter(c => c.timestamp > newerThan).takeLast(limit)
 
     return {
+      schema: 1,
       liveId: this.liveId,
       lastTimestamp: items.last()?.timestamp ?? newerThan,
       isPartial: items.size !== this.chatStore.chatItems.size,
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,6 @@
 export type GenericObject = { [key: string]: any };
+
+// to ensure that the type definitions across different consumers are synced, any changes
+// to the api response schema should be accompanied by a bump of the schema version -
+// this way the consumers can easily detect potential bugs.
+export type ApiSchema<Schema extends number, T> = T & { schema: Schema }
