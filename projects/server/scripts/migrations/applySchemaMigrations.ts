f2b7ad584de2b58244280900e37ed8b856ab4101
Rebel Guy <rebelguysminecraft@gmail.com>
2022-01-08 16:00:59 +1000

    Fixed issue where migration script crashed because of the prisma interactive window requirement

--- projects/server/scripts/migrations/applySchemaMigrations.ts
+++ projects/server/scripts/migrations/applySchemaMigrations.ts
@@ -1,6 +1,8 @@
+require('../../_config')
 import { execAsync } from '../../util/node'
 import { promptInput } from '../../util/input'
 import { getOutstandingMigrationNames } from './util'
+import env from '@rebel/server/globals'
 
 const main = async () => {
   const outstandingMigrationNames = await getOutstandingMigrationNames()
@@ -11,7 +13,20 @@ const main = async () => {
 
     await promptInput('Hit enter to continue, or terminate the script now...')
     console.log('')
-    await execAsync(`yarn prisma migrate dev`)
+
+    const command = `yarn prisma migrate dev`
+    try {
+      await execAsync(`yarn ${command}`)
+    } catch (e) {
+      console.log('')
+      console.log('There was a problem, try running the command manually in the terminal:')
+
+      // don't use dotenv -e because for some reason it ignores the --create-only flag
+      console.log(`  yarn cross-env NODE_ENV=debug DATABASE_URL=${env('databaseUrl')} ${command}`)
+      console.log('')
+      console.log('(you might also want to replace `dev` with `deploy` to force the migration without resetting the data')
+      process.exit(0)
+    }
   } else {
     console.log(`No migrations to apply. Use 'yarn migrate:schema' to create a new migration.`)
     console.log('')
