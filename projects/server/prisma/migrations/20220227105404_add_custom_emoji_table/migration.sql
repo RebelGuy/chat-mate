4419d5559c7ad450bbe0996b618ce2df9a463975
Rebel Guy <rebelguysminecraft@gmail.com>
2022-02-27 20:59:19 +1000

    CHAT-224 Added CustomEmoji table

---
+++ projects/server/prisma/migrations/20220227105404_add_custom_emoji_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `CustomEmoji` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `symbol` VARCHAR(10) NOT NULL,
+    `name` VARCHAR(63) NOT NULL,
+    `image` BLOB NOT NULL,
+    `levelRequirement` SMALLINT UNSIGNED NOT NULL,
+
+    UNIQUE INDEX `CustomEmoji_symbol_key`(`symbol`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
