065a67a8e06c4cc1320a88ff0646dc78a5d8eea6
Rebel Guy <rebelguysminecraft@gmail.com>
2022-03-25 12:14:50 +1000

    Revert "CHAT-269 Forgot to rename index, which resulted in a schema drift issue"
    This reverts commit d24b50d901c4321fc380996448fd43b58a661b6d.

--- projects/server/prisma/migrations/20220324212627_sync_schema_with_db/migration.sql
+++
@@ -1,5 +0,0 @@
--- RenameIndex
-ALTER TABLE `chat_emojis` RENAME INDEX `chat_emoji_youtubeId_key` TO `chat_emojis_youtubeId_key`;
-
--- RenameIndex
-ALTER TABLE `custom_emojis` RENAME INDEX `CustomEmoji_symbol_key` TO `custom_emojis_symbol_key`;
