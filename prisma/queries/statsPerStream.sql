97a07055fa41b555f78321196c563a15ba1bd6dc
Rebel Guy <rebelguysminecraft@gmail.com>
2022-04-03 15:01:38 +1000

    Fixed statsPerStream script

--- prisma/queries/statsPerStream.sql
+++ prisma/queries/statsPerStream.sql
@@ -2,7 +2,7 @@ SELECT
 	stream.createdAt,
     stream.liveId,
     (TIME_TO_SEC(TIMEDIFF(stream.end, stream.start)) / 60) as durationMin,
-    COUNT(msg.id) as msgCount, COUNT(DISTINCT(msg.channelId)) as uniqueAuthors,
+    COUNT(msg.id) as msgCount, COUNT(DISTINCT(msg.userId)) as uniqueAuthors,
     COUNT(msg.id)/(TIME_TO_SEC(TIMEDIFF(stream.end, stream.start)) / 60) as msgPerMin
     
 	FROM chat_mate.chat_message as msg
