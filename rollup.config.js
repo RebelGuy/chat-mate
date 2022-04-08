d9c673994c168a0a585bc7ade4a9cf7021c83a26
Rebel Guy <rebelguysminecraft@gmail.com>
2022-04-09 06:44:41 +1000

    Squashed 'projects/masterchat/' changes from de00ca1..9071443
    9071443 v0.15.0
    503dd36 chore: update docs
    78d3ca8 chore: update changelog
    7faf532  Add @ricecakess as a contributor
    0a3d15c fix: handle empty authorBadges
    f8b0a9b chore: add lockfile
    49f301c fix: handle axios timeout error
    e2de3d1 perf: migrate to axios
    6d08628 fix: `authorName` in `addMembershipItemAction` now becomes optional
    ecfbd53 chore: print error message rather than error code
    64444a6 perf: ditch timeout adjustment
    9f68757 chore: update docs
    61c1fb4 chore: fix version
    fec87a7 0.14.0
    7f6cc85 Merge branch 'dev'
    c137d95 fix: properly handle membership ticker action
    c14275b chore: tidy-up interfaces and docs
    ceef870 fix: poll result now returns YTRun[]
    d619330 feat: add support for membership gift
    b1d4d15 fix: Special handling for anomalous emoji entities
    c30abd0 feat: new action AddPollResultAction
    fd3d177 feat: new API getComment
    6da05f2 chore: update deps
    9695ed8 fix!: normalize replacementItem in ReplaceChatItemAction
    7483e6f fix!: improve AddBannerAction
    180f4d8 chore: fix doc
    e0d736c chore!: move /tools to masterchat-cli
    ff2bf36  Add @stu43005 as a contributor
    7684db5  Add @jprochazk as a contributor
    88ca6eb  Add @uetchy as a contributor
    c943c73 fix: observed empty authorName in membership event
    d193c52 chore: update deps
    4d916ae chore: update deps
    513281c chore: fix typo
    cd3cd14 chore: update docs
    ee91b29 feat!: add comments module
    
    git-subtree-dir: projects/masterchat
    git-subtree-split: 9071443465538c321e07e6c2ebd76e411ceb5f08

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import { terser } from "rollup-plugin-terser";
-import typescript from "rollup-plugin-typescript2";
+// import { terser } from "rollup-plugin-terser";
 
 const isProd = process.env.NODE_ENV === "production";
 
@@ -29,15 +29,15 @@ export default [
         preferBuiltins: false, // required for `events` polyfill
       }),
       commonjs(),
-      isProd &&
-        terser({
-          keep_classnames: true, // avoid Error class mangling
-        }),
+      // isProd &&
+      //   terser({
+      //     keep_classnames: true, // avoid Error class mangling
+      //   }),
     ],
     external: ["cross-fetch", "debug"],
   },
   {
-    input: "./lib/index.d.ts",
+    input: "./lib/lib/index.d.ts",
     output: {
       file: "./lib/masterchat.d.ts",
       format: "es",
