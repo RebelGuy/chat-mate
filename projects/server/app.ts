8b111c994011c257e1422192f40e5a76edf4edc0
Rebel Guy <rebelguysminecraft@gmail.com>
2023-05-31 22:01:26 +1000

    Attempt to force-verify events

--- projects/server/app.ts
+++ projects/server/app.ts
@@ -211,6 +211,11 @@ const main = async () => {
   app.use((req, res, next) => {
     logContext.logInfo('URL called:', req.method, req.path, req.body)
 
+    if (req.method === 'POST' && req.path.startsWith('/event/') && req.body === undefined) {
+      res.sendStatus(200)
+      return
+    }
+
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.header('Access-Control-Allow-Headers', '*')
