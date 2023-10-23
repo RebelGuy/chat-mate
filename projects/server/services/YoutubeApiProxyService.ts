562643afba222129ebd30fc57a091d76ffb8d38c
Rebel Guy <rebelguysminecraft@gmail.com>
2023-10-23 21:11:06 +1000

    CHAT-620 Implemented the youtube API methods

--- projects/server/services/YoutubeApiProxyService.ts
+++ projects/server/services/YoutubeApiProxyService.ts
@@ -1,3 +1,4 @@
+import { youtube_v3 } from '@googleapis/youtube'
 import { YoutubeApiClientProvider } from '@rebel/server/providers/YoutubeApiClientProvider'
 import LogService from '@rebel/server/services/LogService'
 import StatusService from '@rebel/server/services/StatusService'
@@ -12,6 +13,7 @@ type Deps = Dependencies<{
 
 export default class YoutubeApiProxyService extends ApiService {
   private readonly youtubeApiClientProvider: YoutubeApiClientProvider
+  private readonly wrappedApi: (requestOwnerExternalChannelId: string) => Promise<youtube_v3.Youtube>
 
   constructor (deps: Deps) {
     const name = YoutubeApiProxyService.name
@@ -20,23 +22,150 @@ export default class YoutubeApiProxyService extends ApiService {
     const timeout = null
     super(name, logService, statusService, timeout, false)
     this.youtubeApiClientProvider = deps.resolve('youtubeApiClientProvider')
+
+    this.wrappedApi = this.createApiWrapper()
   }
 
-  public async mod (modExternalChannelId: string, userExternalChannelId: string) {
-    const client = await this.youtubeApiClientProvider.getClientForStreamer(modExternalChannelId)
+  public async mod (ownerExternalChannelId: string, userExternalChannelId: string, liveId: string): Promise<string> {
+    const api = await this.wrappedApi(ownerExternalChannelId)
 
-    // todo: wrap api, similar to twurple/masterchat
     // fucking dogshit client typings built diff
     // https://developers.google.com/youtube/v3/live/docs/liveChatModerators/insert#parameters
-    const result = await client.liveChatModerators.insert({
+    const result = await api.liveChatModerators.insert({
+      part: ['snippet'],
       requestBody: {
         snippet: {
           moderatorDetails: {
+            channelId: userExternalChannelId
+          },
+          liveChatId: liveId
+        }
+      }
+    })
+
+    if (result.status !== 200) {
+      // error
+    }
+
+    return result.data.id!
+  }
+
+  public async unmod (ownerExternalChannelId: string, externalModId: string): Promise<void> {
+    const api = await this.wrappedApi(ownerExternalChannelId)
+
+    // https://developers.google.com/youtube/v3/live/docs/liveChatModerators/delete
+    const result = await api.liveChatModerators.delete({
+      id: externalModId
+    })
+
+    if (result.status !== 204) {
+      // error
+    }
+  }
+
+  public async getMods (ownerExternalChannelId: string, liveId: string): Promise<{ externalModId: string, externalChannelId: string }[]> {
+    const api = await this.wrappedApi(ownerExternalChannelId)
+
+    // https://developers.google.com/youtube/v3/live/docs/liveChatModerators/list
+    const result = await api.liveChatModerators.list({
+      part: ['id', 'snippet'],
+      liveChatId: liveId,
+      maxResults: 50
+    })
+
+    if (result.status !== 200) {
+      // error
+    }
+
+    const data = result.data.items!
+    if (data.length >= 40) {
+      this.logService.logWarning(this, `Please implement pagination for the liveChatModerators/list endpoint as we are approaching the limit of 50 (${data.length}).`)
+    }
+
+    return data.map(mod => ({
+      externalModId: mod.id!,
+      // why is everything optional, you fuck
+      externalChannelId: mod.snippet!.moderatorDetails!.channelId!
+    }))
+  }
+
+  public async ban (modExternalChannelId: string, userExternalChannelId: string, liveId: string): Promise<string> {
+    const api = await this.wrappedApi(modExternalChannelId)
+
+    // https://developers.google.com/youtube/v3/live/docs/liveChatBans/insert
+    const result = await api.liveChatBans.insert({
+      part: ['snippet'],
+      requestBody: {
+        snippet: {
+          liveChatId: liveId,
+          type: 'permanent',
+          bannedUserDetails: {
             channelId: userExternalChannelId
           }
         }
       }
     })
 
+    if (result.status !== 200) {
+      // error
+    }
+
+    return result.data.id!
+  }
+
+  public async timeout (modExternalChannelId: string, userExternalChannelId: string, liveId: string, durationSeconds: number): Promise<string> {
+    const api = await this.wrappedApi(modExternalChannelId)
+
+    // https://developers.google.com/youtube/v3/live/docs/liveChatBans/insert
+    const result = await api.liveChatBans.insert({
+      part: ['snippet'],
+      requestBody: {
+        snippet: {
+          liveChatId: liveId,
+          type: 'temporary',
+          bannedUserDetails: {
+            channelId: userExternalChannelId
+          },
+          banDurationSeconds: `${durationSeconds}`
+        }
+      }
+    })
+
+    if (result.status !== 200) {
+      // error
+    }
+
+    return result.data.id!
+  }
+
+  public async unbanOrUntimeout (modExternalChannelId: string, externalPunishmentId: string): Promise<void> {
+    const api = await this.wrappedApi(modExternalChannelId)
+
+    // https://developers.google.com/youtube/v3/live/docs/liveChatBans/insert
+    const result = await api.liveChatBans.delete({
+      id: externalPunishmentId
+    })
+
+    if (result.status !== 204) {
+      // error
+    }
+  }
+
+  private createApiWrapper (): (requestOwnerExternalChannelId: string) => Promise<youtube_v3.Youtube> {
+    return async (requestOwnerExternalChannelId: string) => {
+      const client = await this.youtubeApiClientProvider.getClientForStreamer(requestOwnerExternalChannelId)
+
+      return {
+        liveChatModerators: {
+          list: super.wrapRequest((params: youtube_v3.Params$Resource$Livechatmoderators$List) => client.liveChatModerators.list(params), 'youtube_v3.liveChatModerators.list'),
+          insert: super.wrapRequest((params: youtube_v3.Params$Resource$Livechatmoderators$Insert) => client.liveChatModerators.insert(params), 'youtube_v3.liveChatModerators.insert'),
+          delete: super.wrapRequest((params: youtube_v3.Params$Resource$Livechatmoderators$Delete) => client.liveChatModerators.delete(params), 'youtube_v3.liveChatModerators.delete')
+        },
+        liveChatBans: {
+          insert: super.wrapRequest((params: youtube_v3.Params$Resource$Livechatbans$Insert) => client.liveChatBans.insert(params), 'youtube_v3.liveChatBans.insert'),
+          delete: super.wrapRequest((params: youtube_v3.Params$Resource$Livechatbans$Delete) => client.liveChatBans.delete(params), 'youtube_v3.liveChatBans.delete')
+        }
+      } as youtube_v3.Youtube
+    }
   }
 }
--- projects/server/services/YoutubeService.ts
+++ projects/server/services/YoutubeService.ts
@@ -1,46 +1,102 @@
-import { YoutubeApiClientProvider } from '@rebel/server/providers/YoutubeApiClientProvider'
 import YoutubeApiProxyService from '@rebel/server/services/YoutubeApiProxyService'
 import ChannelStore from '@rebel/server/stores/ChannelStore'
+import LivestreamStore from '@rebel/server/stores/LivestreamStore'
 import StreamerChannelStore from '@rebel/server/stores/StreamerChannelStore'
 import ContextClass from '@rebel/shared/context/ContextClass'
 import { Dependencies } from '@rebel/shared/context/context'
 import { single } from '@rebel/shared/util/arrays'
 
 type Deps = Dependencies<{
-  youtubeApiClientProvider: YoutubeApiClientProvider
   streamerChannelStore: StreamerChannelStore
   channelStore: ChannelStore
   youtubeApiProxyService: YoutubeApiProxyService
+  livestreamStore: LivestreamStore
 }>
 
 export default class YoutubeService extends ContextClass {
-  private readonly youtubeApiClientProvider: YoutubeApiClientProvider
   private readonly streamerChannelStore: StreamerChannelStore
   private readonly channelStore: ChannelStore
   private readonly youtubeApiProxyService: YoutubeApiProxyService
+  private readonly livestreamStore: LivestreamStore
 
   constructor (deps: Deps) {
     super()
-    this.youtubeApiClientProvider = deps.resolve('youtubeApiClientProvider')
     this.streamerChannelStore = deps.resolve('streamerChannelStore')
     this.channelStore = deps.resolve('channelStore')
     this.youtubeApiProxyService = deps.resolve('youtubeApiProxyService')
+    this.livestreamStore = deps.resolve('livestreamStore')
   }
 
   public async getMods (streamerId: number) {
+    const livestream = await this.livestreamStore.getActiveLivestream(streamerId)
+    if (livestream == null) {
+      throw new Error(`Cannot get moderators because streamer ${streamerId} has no active livestream.`)
+    }
+
     const streamerExternalChannelId = await this.getExternalChannelIdFromStreamer(streamerId)
-    const client = await this.youtubeApiClientProvider.getClientForStreamer(streamerExternalChannelId)
+    const mods = await this.youtubeApiProxyService.getMods(streamerExternalChannelId, livestream.liveId)
 
-    // not sure if this is paginated or not... just gonna ignore for now
-    const mods = await client.liveChatModerators.list()
-
-    // todo: transform data into internal type
+    // todo: attach internal ids to items
   }
 
   public async mod (streamerId: number, youtubeChannelId: number) {
+    const livestream = await this.livestreamStore.getActiveLivestream(streamerId)
+    if (livestream == null) {
+      throw new Error(`Cannot add moderator ${youtubeChannelId} because streamer ${streamerId} has no active livestream.`)
+    }
+
     const streamerExternalChannelId = await this.getExternalChannelIdFromStreamer(streamerId)
     const userExternalChannelId = await this.getExtermalChannelIdFromInternalChannelId(youtubeChannelId)
-    const result = await this.youtubeApiProxyService.mod(streamerExternalChannelId, userExternalChannelId)
+    const externalModeratorId = await this.youtubeApiProxyService.mod(streamerExternalChannelId, userExternalChannelId, livestream.liveId)
+    // todo: store externalModeratorId
+  }
+
+  public async unmod (streamerId: number, youtubeChannelId: number) {
+    // todo: get external id
+    const externalModId = ''
+    const streamerExternalChannelId = await this.getExternalChannelIdFromStreamer(streamerId)
+    await this.youtubeApiProxyService.unmod(streamerExternalChannelId, externalModId)
+    // todo: remove externalModeratorId
+  }
+
+  public async ban (streamerId: number, youtubeChannelId: number) {
+    const livestream = await this.livestreamStore.getActiveLivestream(streamerId)
+    if (livestream == null) {
+      throw new Error(`Cannot ban channel ${youtubeChannelId} because streamer ${streamerId} has no active livestream.`)
+    }
+
+    const streamerExternalChannelId = await this.getExternalChannelIdFromStreamer(streamerId)
+    const userExternalChannelId = await this.getExtermalChannelIdFromInternalChannelId(youtubeChannelId)
+    const externalBanId = await this.youtubeApiProxyService.ban(streamerExternalChannelId, userExternalChannelId, livestream.liveId)
+    // todo: store externalBanId
+  }
+
+  public async timeout (streamerId: number, youtubeChannelId: number, durationSeconds: number) {
+    const livestream = await this.livestreamStore.getActiveLivestream(streamerId)
+    if (livestream == null) {
+      throw new Error(`Cannot timeout channel ${youtubeChannelId} because streamer ${streamerId} has no active livestream.`)
+    }
+
+    const streamerExternalChannelId = await this.getExternalChannelIdFromStreamer(streamerId)
+    const userExternalChannelId = await this.getExtermalChannelIdFromInternalChannelId(youtubeChannelId)
+    const externalTimeoutId = await this.youtubeApiProxyService.timeout(streamerExternalChannelId, userExternalChannelId, livestream.liveId, durationSeconds)
+    // todo: store externalTimeoutId
+  }
+
+  public async unban (streamerId: number, youtubeChannelId: number) {
+    // todo: get externalPunishmentId
+    const externalPunishmentId = ''
+    const streamerExternalChannelId = await this.getExternalChannelIdFromStreamer(streamerId)
+    await this.youtubeApiProxyService.unbanOrUntimeout(streamerExternalChannelId, externalPunishmentId)
+    // todo: remove externalPunishmentId
+  }
+
+  public async untimeout (streamerId: number, youtubeChannelId: number) {
+    // todo: get externalPunishmentId
+    const externalPunishmentId = ''
+    const streamerExternalChannelId = await this.getExternalChannelIdFromStreamer(streamerId)
+    await this.youtubeApiProxyService.unbanOrUntimeout(streamerExternalChannelId, externalPunishmentId)
+    // todo: remove externalPunishmentId
   }
 
   private async getExternalChannelIdFromStreamer (streamerId: number) {
