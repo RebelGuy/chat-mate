6232f83148c94c48ce337e44404051d00497d933
Rebel Guy <rebelguysminecraft@gmail.com>
2023-12-14 23:15:01 +1000

    CHAT-708 Final (real) attempt --skip-server --skip-tests

--- projects/studio/fixStudioTypescriptBuild.js
+++ projects/studio/fixStudioTypescriptBuild.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 // when running locally, the TypescriptPerformance file is located in the node_modules of the react-dev-utils package.
 // during CI, it's located directly in the fork-ts-checker-webpack-plugin package.
 
-if (fs.existsSync('../../node_modules/react-dev-utils')) {
-  fs.copyFileSync('./TypeScriptPerformance.js', '../../node_modules/react-dev-utils/node_modules/fork-ts-checker-webpack-plugin/lib/typescript-reporter/profile/TypeScriptPerformance.js')
-} else {
+if (process.env.CI === 'true') {
   fs.copyFileSync('./TypeScriptPerformance.js', '../../node_modules/fork-ts-checker-webpack-plugin/lib/typescript-reporter/profile/TypeScriptPerformance.js')
+} else {
+  fs.copyFileSync('./TypeScriptPerformance.js', '../../node_modules/react-dev-utils/node_modules/fork-ts-checker-webpack-plugin/lib/typescript-reporter/profile/TypeScriptPerformance.js')
 }
