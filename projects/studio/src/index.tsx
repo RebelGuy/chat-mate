8542046fb0d3a2d7d9f033a4daeb6b055f904c5c
Rebel Guy <rebelguysminecraft@gmail.com>
2023-12-29 19:31:45 +1000

    Fixed studio build issue (remnant from previous commit). Testing that this commit indeed fails --skip-server --skip-tests

--- projects/studio/src/index.tsx
+++ projects/studio/src/index.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from '@rebel/studio/App'
-
+f
 const root = createRoot(document.getElementById('root')!)
 
 // note: strict mode invokes lifecycle methods twice so that it's easier to spot bugs. i don't think we want this since our code is bug-free
