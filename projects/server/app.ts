ee485c99d8a4b86b6cba4c5a62f2a49b5a2fb794
Rebel Guy <rebelguysminecraft@gmail.com>
2024-04-29 21:00:03 +1000

    CHAT-775 Fixed local Studio requests being blocked by CORS when talking to a deployed server

--- projects/server/app.ts
+++ projects/server/app.ts
@@ -266,7 +266,7 @@ const main = async () => {
     .build()
 
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', req.headers.origin ?? '*')
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.header('Access-Control-Allow-Headers', '*')
 
