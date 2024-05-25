231342205fb8160bc0d72d0927b90b52597c3f87
Rebel Guy <rebelguysminecraft@gmail.com>
2024-05-25 11:24:49 +1000

    CHAT-787 Added helper text to the ChatMate stats page

--- projects/studio/src/pages/home/ChatMateStats.tsx
+++ projects/studio/src/pages/home/ChatMateStats.tsx
@@ -4,6 +4,7 @@ import AnimatedNumber from '@rebel/studio/components/AnimatedNumber'
 import ApiError from '@rebel/studio/components/ApiError'
 import PanelHeader from '@rebel/studio/components/PanelHeader'
 import RefreshButton from '@rebel/studio/components/RefreshButton'
+import TextWithHelp from '@rebel/studio/components/TextWithHelp'
 import useRequest from '@rebel/studio/hooks/useRequest'
 import useUpdateKey from '@rebel/studio/hooks/useUpdateKey'
 import { getChatMateStats } from '@rebel/studio/utility/api'
@@ -32,12 +33,12 @@ export default function ChatMateStats () {
         </TableRow>
       </TableHead>
       <TableBody>
-        <Stat label="Number of registered users" totalNumber={data?.registeredUserCount ?? 0} />
-        <Stat label="Number of streamers" totalNumber={data?.streamerCount ?? 0} youtubeNumber={data?.youtubeStreamerCount ?? 0} twitchNumber={data?.twitchStreamerCount ?? 0} />
-        <Stat label="Number of unique channels" totalNumber={data?.uniqueChannelCount ?? 0} youtubeNumber={data?.uniqueYoutubeChannelCount ?? 0} twitchNumber={data?.uniqueTwitchChannelCount ?? 0} />
-        <Stat label="Number of chat messages" totalNumber={data?.chatMessageCount ?? 0} youtubeNumber={data?.youtubeMessageCount ?? 0} twitchNumber={data?.twitchMessageCount ?? 0} />
-        <Stat label="Total days livestreamed" totalNumber={data?.totalDaysLivestreamed ?? 0} youtubeNumber={data?.youtubeTotalDaysLivestreamed ?? 0} twitchNumber={data?.twitchTotalDaysLivestreamed ?? 0} decimals={3} />
-        <Stat label="Total experience gained" totalNumber={data?.totalExperience ?? 0} />
+        <Stat label="Number of registered users" help="The number of users that have created a free ChatMate account." totalNumber={data?.registeredUserCount ?? 0} />
+        <Stat label="Number of streamers" help="The number of registered users who have signed up as a ChatMate streamer. Each streamer declares a primary streaming channel on Youtube, Twitch, or both." totalNumber={data?.streamerCount ?? 0} youtubeNumber={data?.youtubeStreamerCount ?? 0} twitchNumber={data?.twitchStreamerCount ?? 0} />
+        <Stat label="Number of unique channels" help="The number of channels that have participated in livestreams connected to ChatMate." totalNumber={data?.uniqueChannelCount ?? 0} youtubeNumber={data?.uniqueYoutubeChannelCount ?? 0} twitchNumber={data?.uniqueTwitchChannelCount ?? 0} />
+        <Stat label="Number of chat messages" help="The number of chat messages that have been received in livestreams connected to ChatMate." totalNumber={data?.chatMessageCount ?? 0} youtubeNumber={data?.youtubeMessageCount ?? 0} twitchNumber={data?.twitchMessageCount ?? 0} />
+        <Stat label="Total days livestreamed" help="The number of days ChatMate streamers have been live for. A livestream represents a continuous span of time during which a streamer was live, either on Youtube, Twitch, or both." totalNumber={data?.totalDaysLivestreamed ?? 0} youtubeNumber={data?.youtubeTotalDaysLivestreamed ?? 0} twitchNumber={data?.twitchTotalDaysLivestreamed ?? 0} decimals={3} />
+        <Stat label="Total experience gained" help="The amount of experience gained by users interacting in livestreams connected to ChatMate. Experience is used for the chat levelling system." totalNumber={data?.totalExperience ?? 0} />
       </TableBody>
     </Table>
   </>
@@ -45,6 +46,7 @@ export default function ChatMateStats () {
 
 type StatProps = {
   label: string
+  help: string
   totalNumber: number
   youtubeNumber?: number
   twitchNumber?: number
@@ -54,7 +56,7 @@ type StatProps = {
 function Stat (props: StatProps) {
   return (
     <TableRow sx={{ m: 1 }}>
-      <Cell style={{ textAlign: 'right' }}>{props.label}</Cell>
+      <Cell style={{ textAlign: 'right' }}><TextWithHelp text={props.label} help={props.help} disableInteractive arrow /></Cell>
       <Cell><Number target={props.totalNumber} decimals={props.decimals} /></Cell>
       <Cell><Number target={props.youtubeNumber} decimals={props.decimals} /></Cell>
       <Cell><Number target={props.twitchNumber} decimals={props.decimals} /></Cell>
