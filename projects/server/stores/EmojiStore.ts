13c130ed868a4a510346ffb68693d57c53297963
Rebel Guy <rebelguysminecraft@gmail.com>
2024-07-30 20:54:28 +1000

    CHAT-809 Fixed chat messages with multiple new emojis sometimes failing to process

--- projects/server/stores/EmojiStore.ts
+++ projects/server/stores/EmojiStore.ts
@@ -6,6 +6,7 @@ import { ImageInfo } from '@rebel/server/stores/CustomEmojiStore'
 import ContextClass from '@rebel/shared/context/ContextClass'
 import { Dependencies } from '@rebel/shared/context/context'
 import { GroupedSemaphore } from '@rebel/shared/util/Semaphore'
+import { randomString } from '@rebel/shared/util/random'
 
 type Deps = Dependencies<{
   dbProvider: DbProvider
@@ -50,7 +51,7 @@ export default class EmojiStore extends ContextClass {
         name: chatEmojiMessage.name ?? null,
         isCustomEmoji: false,
         image: { create: {
-          fingerprint: `TEMP-${Date.now()}`,
+          fingerprint: `TEMP-${randomString(12)}`,
           url: 'TEMP',
           width: 0,
           height: 0
